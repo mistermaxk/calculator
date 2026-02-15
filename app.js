f9ed20fa76bed895a60ba86a581516de98dad607
Max K <mxkk@pm.me>
2026-02-15 14:43:50 -0500

    Create basic operation functions

---
+++ app.js
@@ -0,0 +1,18 @@
+/* Calculator
+   Max K @mistermaxk 2026 */
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
