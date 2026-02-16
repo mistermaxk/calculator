303f06b51ba335dcda281dc4407068bba0d8f504
Max K <mxkk@pm.me>
2026-02-16 13:06:06 -0500

    Add event listeners

--- app.js
+++ app.js
@@ -23,3 +23,24 @@ function divide(a, b) {
 function operate(operandA, operandB, operator) {
     return operator(operandA, operandB);
 }
+
+const display = document.querySelector("#display");
+
+const numeralsBtn = document.querySelector(".numerals");
+numeralsBtn.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+        operandA = Number(e.target.textContent);
+        display.textContent = operandA;
+    }
+});
+
+const operatorsBtn = document.querySelector(".operators");
+operatorsBtn.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+});
+
+const auxiliaryBtn = document.querySelector(".auxiliary");
+auxiliaryBtn.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+    console.log(e)
+});
